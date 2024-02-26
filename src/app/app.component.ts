2ffe2efc4c9c41d151cf4d484b0532d0f736badc
veikkahonkanen <veikka.honkanen@gmail.com>
2024-02-26 15:56:23 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, NgForm } from "@angular/forms";
+import { User } from "./user";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-template-driven-and-reactive-forms-2023';
+  title = 'skills';
+  user: User = {};
+
+  constructor() {
+    this.user = new User();
+  }
+
+  onSubmit(userDetails: User /*NgForm*/) {
+    // console.log("Form submitted! ", userDetails.value.fName);
+    // console.log(`First name: ${userDetails.controls["fName"].value} Last name: ${userDetails.controls["lName"].value}`);
+    console.log(userDetails);
+  }
 }
